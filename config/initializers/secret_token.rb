e6c08a52556d93e03f7da0146aa95954861b67b5
Abhinav Trivedi <abhinavtrivedi@gmail.com>
2014-01-15 01:11:30 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GuessingGameApp::Application.config.secret_key_base = '441f635c1fa21b8d808bc4161332fe32a879c9602e38d7bbb85191fae0ea7f56598ddd181240697648ded210d638a8a56f4de7f61a5d944e73ae79e854551fb0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GuessingGameApp::Application.config.session_store :cookie_store, key: '_guessing_game_app_session'
